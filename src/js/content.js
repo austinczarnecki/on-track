26758711231bc43e3e2a35e1b3559518cdd3a035
Austin Czarnecki <austin.czarnecki@gmail.com>
2016-04-09 04:25:31 -0700

    removed debugging code

--- src/js/content.js
+++ src/js/content.js
@@ -3,12 +3,8 @@
 'use strict';
 
 window.addEventListener("blur", function() {
-	console.log('focus changed on blur');
 	chrome.runtime.sendMessage('blur');
 });
 window.addEventListener('focus', function() {
-	console.log('focus gained')
 	chrome.runtime.sendMessage('focus');
 });
-
-console.log('context script loaded');
