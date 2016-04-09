b0350284365bd584a612f53002c965d1db3e5fd4
Austin Czarnecki <austin.czarnecki@gmail.com>
2016-04-09 04:24:26 -0700

    fixed bugs with tracking and saving time spent on the active tab

--- src/js/background.js
+++ src/js/background.js
@@ -18,9 +18,9 @@ function clearActiveTab() {
 	activeTab = activeStart = activeDomain = null;
 }
 
-function setActiveStart(tab) {
+function setActiveStart(domain) {
 	activeStart = Date.now();
-	if (verbose) { console.log("new active tab: " + tab.id + " : " + extractDomain(tab.url)); }
+	if (verbose) { console.log("new active tab: " + domain); }
 }
 
 // takes a url and a value in miliseconds and adds that value to the 
@@ -28,49 +28,43 @@ function setActiveStart(tab) {
 function storeIncrement(domain, time) {
 	time = parseInt(time);
 
-	try { 
+	try {
 		storage.addTimeSpentToDomain(domain, time);
 	} catch(err) {
 		storage.addTimeSpentToDomain(domain, time);
 	}
 }
 
-function storeTotalActiveTab(callback) {
-	var diff = Date.now() - activeStart;
-	if (activeDomain) {
-		storeIncrement(activeDomain, diff);
-		if (verbose) { console.log("stored increment: id="+activeTab+", url="+activeDomain+", time="+(diff/1000)+" seconds"); }
-	}
-	callback();
-}
+function storeTotalActiveTab(domain, start, tabId) {
+	var diff = Date.now() - start;
+	if (domain) { storeIncrement(domain, diff); }
+};
 
 /* ===================================================== */
 // WINDOW LISTENERS
 
 // when closing a window, save and clear the active tab if set
 chrome.windows.onRemoved.addListener(function(windowId) {
-	if (activeTab) {
-		storeTotalActiveTab(function() {
-			clearActiveTab();
-		});
+	if (activeDomain) {
+		storeTotalActiveTab(activeDomain, activeStart, activeTab);
+		clearActiveTab();
 	}
 });
 
 // when window enters or leaves focus, save and clear active tab if set, and try
 // to set the active tab to the active tab of the window that came into focus 
 chrome.windows.onFocusChanged.addListener(function(windowId) {
-	if (activeTab) {
-		storeTotalActiveTab(function() {
-			clearActiveTab();
-		})
+	if (activeDomain) {
+		storeTotalActiveTab(activeDomain, activeStart, activeTab);
+		clearActiveTab();
 	} else {
 		chrome.tabs.query({ active: true, lastFocusedWindow: true }, function (tabs) {
 			if (tabs.length < 1) {
 				if (verbose) { console.log('No active tabs found'); }
 		    } else {
 				activeTab = tabs[0].id;
-				setActiveStart(tabs[0]);
 				activeDomain = extractDomain(tabs[0].url);
+				setActiveStart(activeDomain);
 			}
 		});
 	}
@@ -82,25 +76,24 @@ chrome.windows.onFocusChanged.addListener(function(windowId) {
 // if the updated tab is the active tab and domain changed, save out the time for the 
 // previous domain, reset the counter for the active tab. Else do nothing.
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-	if (activeTab) {
-		storeTotalActiveTab(function() {
-			activeDomain = extractDomain(tab.url);
-			setActiveStart(tab);
-		});
+	if (activeDomain) {
+		storeTotalActiveTab(activeDomain, activeStart, activeTab);
+		activeDomain = extractDomain(tab.url);
+		activeTab = tabId;
+		setActiveStart(activeDomain);
 	}
 });
 
 // when a tab becomes active, save any existing active tab and set the new
 // tab to be the active tab.
 chrome.tabs.onActivated.addListener(function(activeInfo) {
-	if (activeTab) { 
-		storeTotalActiveTab(function() {
-			clearActiveTab();
-			activeTab = activeInfo.tabId;
-			chrome.tabs.get(activeTab, function(tab) {
-				activeDomain = extractDomain(tab.url);
-				setActiveStart(tab);
-			});
+	if (activeDomain) {
+		storeTotalActiveTab(activeDomain, activeStart, activeTab);
+		clearActiveTab();
+		activeTab = activeInfo.tabId;
+		chrome.tabs.get(activeTab, function(tab) {
+			activeDomain = extractDomain(tab.url);
+			setActiveStart(activeDomain);
 		});
 	} else {
 		chrome.tabs.get(activeInfo.tabId, function(tab) {
@@ -108,9 +101,9 @@ chrome.tabs.onActivated.addListener(function(activeInfo) {
 		        console.log(chrome.runtime.lastError.message);
 		    } else {
 		    	clearActiveTab();
-		    	activeTab = activeInfo.tabId;
+		    	activeTab = tab.id;
 				activeDomain = extractDomain(tab.url);
-				setActiveStart(tab);
+				setActiveStart(activeDomain);
 		    }
 		});
 	} 
@@ -119,19 +112,15 @@ chrome.tabs.onActivated.addListener(function(activeInfo) {
 // listen to messages from content script notifying if page goes out of 
 // view. This uses the blur and focus events.
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
-	if (activeTab) {
-		storeTotalActiveTab(function() {
-			clearActiveTab();
-			if (message === 'focus') {
-				activeTab = sender.tab.id;
-				activeDomain = extractDomain(sender.url);
-				setActiveStart(sender.tab);
-			}
-		});
-	} else if (message === 'focus') {
+	if (activeDomain) {
+		storeTotalActiveTab(activeDomain, activeStart, activeTab);
+		clearActiveTab();
+	} 
+	if (message === 'focus') {
+		console.log(sender)
 		activeTab = sender.tab.id;
-		activeDomain = extractDomain(sender.url);
-		setActiveStart(sender.tab);
+		activeDomain = extractDomain(sender.tab.url);
+		setActiveStart(activeDomain);
 	}
 });
 
